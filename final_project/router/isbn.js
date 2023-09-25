16c1b9c62119dcc2831a84d5a0672cb06eb476ae
pallapoluabhhiram <108801196+pallapoluabhhiram@users.noreply.github.com>
2023-09-25 08:29:57 +0530

    Create isbn.js

---
+++ final_project/router/isbn.js
@@ -0,0 +1,13 @@
+let isbn={
+   1: {"author": "Chinua Achebe","title": "Things Fall Apart","isbn code":"1231"},
+   2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn code":"1232"},
+   3: {"author": "Dante Alighieri","title": "The Divine Comedy", "isbn code":"1233"},
+   4: {"author": "Unknown","title": "The Epic Of Gilgamesh",  "isbn code":"1234"},
+   5: {"author": "Unknown","title": "The Book Of Job",  "isbn code":"1235"},
+  6: {"author": "Unknown","title": "One Thousand and One Nights","isbn code":"1236"},
+  7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn code":"1237"},
+  8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn code":"1238"},
+  9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot","isbn code":"1239"},
+  10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn code":"12310" }
+}
+module.exports=isbn;
